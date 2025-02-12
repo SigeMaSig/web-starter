2949cc94b5938d4b20e6ce4869ddcb4517bd27b3
Aj kie <sigema.soulerror>
2025-02-12 13:09:33 +0700

    Add Errors

--- Learn/LearnRegister/main.js
+++ Learn/LearnRegister/main.js
@@ -1,13 +1,38 @@
-function submitData(){
+const validatedata = (userData) =>{
+    let errors = []
+    if(!userData.firstname){
+        errors.push('กรุณาใส่ชื่อจริง')
+    }
+    if(!userData.lastname){
+        errors.push('กรุณาใส่นามสกุล')
+    }
+    if(!userData.age){
+        errors.push('กรุณาใส่อายุ')
+    }
+    if(!userData.gender){
+        errors.push('กรุณาใส่เพศ')
+    }
+    if(!userData.interest){
+        errors.push('กรุณาใส่สิ่งที่สนใจ')
+    }
+    if(!userData.description){
+        errors.push('กรุณาใส่รายละเอียด')
+    }
+    return errors
+}
+const submitData =  async() => {
     let fristnameDom = document.querySelector('input[name=firstname]')
     let lastnameDom = document.querySelector('input[name=lastname]')
     let ageDom = document.querySelector('input[name=age]')
 
-    let genderDom = document.querySelector('input[name=gender]:checked')
-    let interestDoms = document.querySelectorAll('input[name=interest]:checked ')
+    let genderDom = document.querySelector('input[name=gender]:checked') || {}
+    let interestDoms = document.querySelectorAll('input[name=interest]:checked ') ||{} 
     
     let discriptionDom = document.querySelector('textarea[name=description]')
 
+    let messageDOM = document.getElementById('message')
+
+    try{
     let interest = ''
     
     for (let i = 0 ; i<interestDoms.length;i++ ){
@@ -16,14 +41,47 @@ function submitData(){
             interest += ','
         }
     }
-
     let userData = {
-        fristname: fristnameDom.value,
+        firstname: fristnameDom.value,
         lastname: lastnameDom.value,
         age: ageDom.value,
         gender: genderDom.value,
-        discription:discriptionDom.value,
+        description:discriptionDom.value,
         interest: interest
     }
-    console.log(userData)
+
+        console.log('Submit data' ,userData)
+
+        const errors = validatedata(userData)
+        if (errors.length>0){
+            throw{
+                message:'กรอกข้อมูลไม่ครบ',
+                errors:errors
+            }
+        }
+
+        const response = await axios.post('http://localhost:8000/user',userData)
+        console.log('response',response.data)
+        messageDOM.innerHTML = 'บันทึกข้อมูล'
+        messageDOM.className = 'message suscess'
+    }
+
+        catch(error){
+            console.log('error message',error.message)
+            console.log('error',error.errors)
+            // if (error.response)
+            // console.log(error.response.data.message)
+            
+            let htmlData = '<div>'
+            htmlData += `<div>${error.message}</div>`
+            htmlData += '<ul>'
+            for (let i=0 ; i < error.errors.length;i++){
+                htmlData += `<li>${error.errors[i]}</li>`
+            }
+            htmlData += '</ul>'
+            htmlData += '</div>'
+
+            messageDOM.innerHTML = htmlData
+            messageDOM.className = 'message danger'
+        }
 }
--- Learn/LearnRegister/server/index.js
+++ Learn/LearnRegister/server/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const bodyparser = require('body-parser')
 const mysql = require('mysql2/promise')
+const cors = require('cors')
 const app = express()
-
+app.use(cors())
 app.use(bodyparser.json()) // ส่งเป็น Text ออกมา
 
 const port = 8000
@@ -37,6 +38,43 @@ app.post('/user',async(req,res)=>{
     res.json({
       message : 'insert ok',
       data : results[0]
+    })}
+    catch (error){
+      res.status(500).json({
+        message : 'Someing wrong'
+      })
+    }
+})
+
+//GET /users/:id สำหรับการดึง users รายคนออกมา
+app.get('/users/:id', async(req,res)=>{
+  try {
+    let id = req.params.id
+  const results = await conn.query('SELECT * FROM user WHERE id = ?',id)
+    if(results[0].length == 0){
+      throw { statusCode:404, message:'หาไม่เจอ'}
+    }
+    res.json(results[0][0])
+  } 
+  catch (error) {
+    console.log(error.message)
+    let statusCode = error.statusCode || 500
+      res.status(statusCode).json({
+        message : 'Someing wrong',
+        errorMessage: error.message
+      })}
+})
+
+// path = PUT/user/:id สำหรับการแก้ไข users รายคน (ตาม id ที่บันทึกเข้าไป)
+app.put('/users/:id',async(req,res)=>{
+  try{
+    let id = req.params.id
+    let updateUser = req.body  
+    const results = await conn.query('UPDATE user SET ? WHERE id = ?' , [updateUser,id])
+  
+    res.json({
+      message : 'update ok',
+      data : results[0]
     })}catch (error){
       console.log(error.message)
       res.status(500).json({
@@ -45,52 +83,21 @@ app.post('/user',async(req,res)=>{
     }
 })
 
-//GET /users/:id สำหรับการดึง users รายคนออกมา
-app.get('/users/:id',(req,res)=>{
-  let id = req.params.id
-  let selectrdindex = users.findIndex(user=> user.id == id)
-
-  res.json(users[selectrdindex])
-})
-
-// path = PUT/user/:id สำหรับการแก้ไข users รายคน (ตาม id ที่บันทึกเข้าไป)
-app.put('/users/:id',(req,res)=>{
-  let id = req.params.id
-  let updateUser = req.body
-
-  // หา users จาก id ที่ส่งมา
-  let selectrdindex = users.findIndex(user=> user.id == id)
-  // update users นั้น
-
-    users[selectrdindex].firstname = updateUser.firstname || users[selectrdindex].firstname
-    users[selectrdindex].lastname = updateUser.lastname || users[selectrdindex].lastname
-    users[selectrdindex].age = updateUser.age || users[selectrdindex].age
-    users[selectrdindex].gender = updateUser.gender || users[selectrdindex].gender
-
-  res.json({
-    massage : 'update user complate!!!',
-    data:{
-      user :updateUser,
-      indexUpdate: selectrdindex
-    }
-  })
-})
-
 //path DELETE /user/:id สำหรับการลบ users รายคน (ตาม id ที่บันทึกเข้าไป)
-app.delete('/users/:id',(req,res)=>{
-  let id = req.params.id
-  // หา index ของ user ที่จะลบ คืออันไหน
-  let selectrdindex = users.findIndex(user=> user.id == id)
-
-  //ลบ
-  //delete users[selectrdindex]
-
-  users.splice(selectrdindex,1)
-
-  res.json({
-    massage: 'delete complete',
-    indexDelete : selectrdindex
-  })
+app.delete('/users/:id',async(req,res)=>{
+  try{
+    let id = req.params.id  
+    const results = await conn.query('DELETE from user WHERE id = ?' ,id)
+  
+    res.json({
+      message : 'delete ok',
+      data : results[0]
+    })}catch (error){
+      console.log(error.message)
+      res.status(500).json({
+        message : 'Someing wrong'
+      })
+    }
 })
 
 app.listen(port,async(req,res)=>{
